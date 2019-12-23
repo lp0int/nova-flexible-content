803d6c7ab0abf7ccd48dd4662bfdd20be81db4d7
lpoint <xiaoawl23@hotmail.com>
2019-12-23 15:42:35 +0800

    “init”

---
+++ src/FieldServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Whitecube\NovaFlexibleContent;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+use Whitecube\NovaFlexibleContent\Commands\CreateLayout;
+use Whitecube\NovaFlexibleContent\Commands\CreatePreset;
+use Whitecube\NovaFlexibleContent\Commands\CreateResolver;
+use Whitecube\NovaFlexibleContent\Http\Middleware\InterceptFlexibleAttributes;
+
+class FieldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['router']->pushMiddlewareToGroup('nova', InterceptFlexibleAttributes::class);
+        
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('nova-flexible-content', __DIR__.'/../dist/js/field.js');
+            Nova::style('nova-flexible-content', __DIR__.'/../dist/css/field.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (!$this->app->runningInConsole()) return;
+
+        $this->commands([
+            CreateLayout::class,
+            CreatePreset::class,
+            CreateResolver::class,
+        ]);
+    }
+}
